83b2c9ed0c5ca9857870063415dc25f6adf1aa26
Shaked Yona <shakedyona123@gmail.com>
2019-03-25 18:36:35 +0200

    Commit for explanation

--- Baseline.py
+++ Baseline.py
@@ -1,6 +1,7 @@
 import Preprocessing.preprocessing as pre
 import pandas as pd
 import utils
+import Performances.performances as per
 
 path = 'data.csv'
 offensive_words = 'offensive_words.csv'
@@ -10,16 +11,14 @@ tagged_df = utils.get_tagged_posts(df)
 df_offensive = pd.read_csv(offensive_words, names=['words'])
 offensive = df_offensive['words'].tolist()
 df_compare = pd.DataFrame(columns=['Text', 'Algorithm classify', 'Original classify'])
-pre.find_df(tagged_df, 0.8)
 for index, row in tagged_df.iterrows():
-    print(row.text)
     tokens = pre.tokenize_post(str(row.text))
     if any(elem in tokens for elem in offensive):
-        df_compare.append({'Text': row['text'], 'Algorithm classify': '3', 'Original classify': row['cb_level']})
+        df_compare = df_compare.append({'Text': row['text'], 'Algorithm classify': '3', 'Original classify': row['cb_level']}, ignore_index = True)
     else:
-        df_compare.append({'Text': row['text'], 'Algorithm classify': '1', 'Original classify': row['cb_level']})
-print(df_compare)
-
+        df_compare = df_compare.append({'Text': row['text'], 'Algorithm classify': '1', 'Original classify': row['cb_level']}, ignore_index = True)
+f1,prec,recall = per.get_performances(df_compare['Original classify'].tolist(),df_compare['Algorithm classify'].tolist())
+print(f1,prec,recall)
 
 # tagged_df = pre.remove_stop_words(tagged_df, pre.get_stop_words())
 # todo: tokenize, rule for each text- if one of the words from offensive ia there. after- present the score for the model
