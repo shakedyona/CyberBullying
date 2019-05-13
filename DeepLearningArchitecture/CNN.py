9560dd11651d5b73f35b82f91deebe64044540a0
Sivan Rejensky <>
2019-05-13 18:55:29 +0300

    CNN and LSTM

--- DeepLearningArchitecture/CNN.py
+++ DeepLearningArchitecture/CNN.py
@@ -2,29 +2,27 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import RandomizedSearchCV
 from keras.models import Sequential
 from keras import layers
-import matplotlib.pyplot as plt
 from DeepLearningArchitecture.DLModel import DLModel
 
 
 class CNN(DLModel):
-    def __init__(self, embedding_matrix=None, model=None):
-        super().__init__(embedding_matrix)
-        self.model = model
+    def __init__(self, dataframe, embedding_matrix=None, model=None):
+        super().__init__(dataframe, embedding_matrix, model)
 
     def create_model(self, num_filters, kernel_size):
         model = Sequential()
         model.add(self.word2vec_embedding_layer())
         model.add(layers.Conv1D(num_filters, kernel_size, activation='relu'))
         model.add(layers.GlobalMaxPooling1D())
-        model.add(layers.Dense(10, activation='relu'))
+        model.add(layers.Dense(20, activation='relu'))
         model.add(layers.Dense(1, activation='sigmoid'))
         model.compile(optimizer='adam',
-                      loss='binary_crossentropy',
-                      metrics=['accuracy'])
+                      loss='mse',
+                      metrics=['accuracy', 'mae', 'mean_squared_error'])
         self.model = model
         return model
 
-    def train(self, x_train, y_train, x_test, y_test, epochs=50, verbose=False, batch_size=10):
+    def train(self, x_train, y_train, x_test, y_test, epochs=30, batch_size=10, verbose=True):
         history = self.model.fit(x_train, y_train,
                                  epochs=epochs,
                                  verbose=verbose,
@@ -35,13 +33,10 @@ class CNN(DLModel):
     def predict(self, X):
         pass
 
-    def run_grid_search(self, X_train, y_train, X_test, y_test, vocab_size, embedding_dim, maxlen, epochs):
+    def run_grid_search(self, X_train, y_train, X_test, y_test, epochs):
         # Parameter grid for grid search
         param_grid = dict(num_filters=[32, 64, 128],
-                          kernel_size=[3, 5, 7],
-                          vocab_size=[vocab_size],
-                          embedding_dim=[embedding_dim],
-                          maxlen=[maxlen])
+                          kernel_size=[3, 5, 7])
         model = KerasClassifier(build_fn=self.create_model,
                                 epochs=epochs, batch_size=10,
                                 verbose=False)
@@ -61,31 +56,3 @@ class CNN(DLModel):
             grid_result.best_params_,
             test_accuracy)
         return output_string
-
-    def print_evaluation(self, history, X_train, y_train, X_test, y_test, verbose=False):
-        loss, accuracy = self.model.evaluate(X_train, y_train, verbose=verbose)
-        print("Training Accuracy: {:.4f}".format(accuracy))
-        loss, accuracy = self.model.evaluate(X_test, y_test, verbose=verbose)
-        print("Testing Accuracy:  {:.4f}".format(accuracy))
-        plot_history(history)
-
-
-def plot_history(history):
-    plt.style.use('ggplot')
-    acc = history.history['acc']
-    val_acc = history.history['val_acc']
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    x = range(1, len(acc) + 1)
-
-    plt.figure(figsize=(12, 5))
-    plt.subplot(1, 2, 1)
-    plt.plot(x, acc, 'b', label='Training acc')
-    plt.plot(x, val_acc, 'r', label='Validation acc')
-    plt.title('Training and validation accuracy')
-    plt.legend()
-    plt.subplot(1, 2, 2)
-    plt.plot(x, loss, 'b', label='Training loss')
-    plt.plot(x, val_loss, 'r', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.legend()
--- DeepLearningArchitecture/DLModel.py
+++ DeepLearningArchitecture/DLModel.py
@@ -1,12 +1,17 @@
 import numpy as np
 from keras.layers import Embedding
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import matplotlib.pyplot as plt
 
 
 class DLModel:
-    def __init__(self, embedding_matrix=None):
+    def __init__(self, dataframe, embedding_matrix=None, model=None):
+        self.dataframe = dataframe
         self.embedding_matrix = embedding_matrix
+        self.model = model
 
-    def create_input_from_embedding(self, model):
+    def create_embedding_matrix(self, model):
         if self.embedding_matrix is not None:
             return
         embedding_matrix = np.zeros((len(model.wv.vocab), 100))
@@ -23,3 +28,41 @@ class DLModel:
                           output_dim=self.embedding_matrix.shape[1],
                           weights=[self.embedding_matrix])
         return layer
+
+    def create_data_input(self):
+        posts = self.dataframe['text'].tolist()
+        t = Tokenizer()
+        t.fit_on_texts(posts)
+
+        encoded_docs = t.texts_to_sequences(posts)
+
+        max_length = 200
+        return pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+
+    def print_evaluation(self, history, X_train, y_train, X_test, y_test, verbose=False):
+        (loss, accuracy, mae,  mse) = self.model.evaluate(X_train, y_train, verbose=verbose)
+        print("Training Accuracy: {:.4f}".format(accuracy))
+        (loss, accuracy, mae, mse) = self.model.evaluate(X_test, y_test, verbose=verbose)
+        print("Testing Accuracy:  {:.4f}".format(accuracy))
+        plot_history(history)
+
+
+def plot_history(history):
+    plt.style.use('ggplot')
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Training acc')
+    plt.plot(x, val_acc, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
