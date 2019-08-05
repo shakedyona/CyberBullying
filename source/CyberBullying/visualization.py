f023691834e231824a4969ab942a7d5968c0499b
Sivan Rejensky <sivanr@rezilion.com>
2019-08-05 15:04:54 +0300

    documentation of visualisation

--- source/CyberBullying/visualization.py
+++ source/CyberBullying/visualization.py
@@ -9,19 +9,13 @@ import pathlib
 WHERE_OUTPUTS = pathlib.Path(__file__).parent
 
 
-def plot_tf_idf_post(dictionary_tf_idf, title):
-    dic_post = dict(dictionary_tf_idf[title])
-    dic_post_travers = {}
-    for term,val in dic_post.items():
-        dic_post_travers[utils.traverse(term)] = val
-    df2 = pd.DataFrame.from_dict(dic_post_travers,orient='index').sort_values(by=0, ascending=False)
-    pl = df2.plot(kind='bar', figsize=(15,7), fontsize=8, legend=False, title=utils.traverse(title))
-    for p in pl.patches:
-        pl.annotate(str(p.get_height()), (p.get_x() * 0.98, p.get_height() * 1.001), fontsize=14)
-    plt.show()
-
-
-def plot_length_posts(dictionary_length, title):
+def plot_dictionary(dictionary_length, title):
+    """
+    plot dictionary with hebrew keys support
+    :param dictionary_length:
+    :param title:
+    :return:
+    """
     df2 = pd.DataFrame.from_dict(dictionary_length, orient='index').sort_values(by=0, ascending=False)
     pl = df2.plot(kind='bar', figsize=(15, 7), fontsize=8, legend=False, title=utils.traverse(title))
     for p in pl.patches:
@@ -30,6 +24,13 @@ def plot_length_posts(dictionary_length, title):
 
 
 def create_word_cloud(no_topics, lda, feature_names):
+    """
+    create picture with words in different size according to frequency
+    :param no_topics:
+    :param lda:
+    :param feature_names:
+    :return:
+    """
     logger = Logger.get_logger_instance()
     font_path = os.path.join(os.path.join(os.environ['WINDIR'], 'Fonts'), 'ahronbd.ttf')
     for i in range(0, no_topics):
@@ -44,29 +45,27 @@ def create_word_cloud(no_topics, lda, feature_names):
 
 
 def create_lda_visualization(no_topics, lda_model):
+    """
+    create a word cloud for each topic from an lda model.
+    :param no_topics:
+    :param lda_model:
+    :return:
+    """
     tf_vectorizer = utils.get_model(os.path.join(WHERE_OUTPUTS / 'outputs', 'tf.pkl'))
     tf_feature_names = tf_vectorizer.get_feature_names()
 
     create_word_cloud(no_topics, lda_model, tf_feature_names)
 
 
-def print_tf_idf_dict(tf_idf_dict):
-    for key, value in tf_idf_dict.items():
-        print('post: ')
-        print(key)
-        for v in value:
-            print('word: ' + str(v[0]) + ', tf-idf: ' + str(v[1]))
-
-
-def plot_part_of_day(dictionary_time, title):
-    df2 = pd.DataFrame.from_dict(dictionary_time, orient='index').sort_values(by=0, ascending=False)
-    pl = df2.plot(kind='bar', figsize=(15, 7), fontsize=8, legend=False, title=utils.traverse(title))
-    for p in pl.patches:
-        pl.annotate(str(p.get_height()), (p.get_x() * 0.98, p.get_height() * 1.001), fontsize=14)
-    plt.show()
-
-
 def plot_roc_curve(roc_auc, fpr, tpr, name):
+    """
+    plot ROC curve graph with a given auc, false positive rate, true positive rate and a graph title (name)
+    :param roc_auc:
+    :param fpr:
+    :param tpr:
+    :param name:
+    :return:
+    """
     plt.figure()
     lw = 1
     plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
@@ -81,12 +80,26 @@ def plot_roc_curve(roc_auc, fpr, tpr, name):
 
 
 def plot_feature_importance_xgb(booster):
+    """
+    plot XGBoost feature importance of a given XGBoost booster
+    :param booster:
+    :return:
+    """
     xgb.plot_importance(booster, importance_type='gain')
     plt.rcParams['figure.figsize'] = [5, 5]
     plt.show()
 
 
 def plot_models_compare(per1, per2, per3, per4):
+    """
+    plot a bar graph for comparison of the 4 models: Baseline, XGBoost, Random forest and Naive bayes
+    compare values of precision recall and f-measure
+    :param per1:
+    :param per2:
+    :param per3:
+    :param per4:
+    :return:
+    """
     fig, ax = plt.subplots()
     index = np.arange(3)
     ax.bar(index, [per1[key] for key in sorted(per1.keys())], color=(0.5, 0.4, 0.8, 0.4), width=0.2, label='Baseline')
