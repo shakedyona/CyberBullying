626a77287aa50bea3de2681c9160c467bfb76144
Shaked Yona <shakedyona123@gmail.com>
2019-08-06 13:57:01 +0300

    main

--- source/main.py
+++ source/main.py
@@ -3,7 +3,7 @@ import os.path
 SOURCE = os.path.join(__file__, '../')
 
 api.train(os.path.join(SOURCE, 'dataNew.csv'))
-pred = api.get_classification('וואי שמלה בת זונה', True)
+pred = api.get_classification('וואי את ילדה מאוד מטומטמת', True)
 print(pred)
 performances = api.get_performance(os.path.join(SOURCE, 'dataNew.csv'))
 print(performances)
